757ef7e3db57418530567f51aecc028988907e4d
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-04-14 11:37:41 -0300

    add other debug

--- api/sync.js
+++ api/sync.js
@@ -27,9 +27,9 @@ export default async function handler(req, res) {
 
   if (req.method === 'GET') {
     try {
+      console.log('URL:', GOOGLE_SHEETS_URL)
       const response = await fetch(GOOGLE_SHEETS_URL)
       const data = await response.json()
-      console.log('URL:', GOOGLE_SHEETS_URL)
       return res.status(200).json(data)
     } catch (err) {
       return res.status(500).json({ error: 'Erro ao buscar dados' })
